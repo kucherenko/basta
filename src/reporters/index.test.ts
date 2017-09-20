5ffdbbe6d253c91c2c75f7b527efda0caa95828f
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2017-09-20 12:13:01 +0300

    feature(test) add tests for reporters registration

--- src/reporters/index.test.ts
+++ src/reporters/index.test.ts
@@ -1,10 +1,41 @@
-import {expect} from 'chai';
+import * as proxyquire from 'proxyquire';
 
 describe('CPD: Reporters', () => {
 
-    it('should exists', () => {
-        const a = true;
-        expect(a).equal(true);
+    let sut;
+    let name;
+    let reporter;
+
+    beforeEach(() => {
+        name = 'test';
+        reporter = {};
+        sut = proxyquire('.', {});
+    });
+
+    it('should register reporter', () => {
+        sut.registerReporter(name, reporter);
+        sut.getRegisteredReporters()[name].should.equal(reporter);
+    });
+
+    it('should check is reporter registered', () => {
+        sut.registerReporter(name, reporter);
+        sut.hasReporter(name).should.equal(true);
+    });
+
+    it('should return registered reporter', () => {
+        sut.registerReporter(name, reporter);
+        sut.getReporter(name).should.equal(reporter);
+    });
+
+    context('Registered Reporters', () => {
+
+        it('should register html reporter', () => {
+            sut.hasReporter('html').should.equal(true);
+        });
+
+        it('should register console reporter', () => {
+            sut.hasReporter('console').should.equal(true);
+        });
     });
 
 });
